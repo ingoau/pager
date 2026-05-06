de5be5b56341acf62a10b54ecdee6e73c37caaf8
Ingo Wolf <me@ingo.au>
2026-05-07 00:10:06 +1000

    add basic form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import Input from '$lib/components/ui/input/input.svelte';
+	import Textarea from '$lib/components/ui/textarea/textarea.svelte';
+</script>
+
+<div class="w-full p-4">
+	<div class="mx-auto w-full max-w-xl space-y-2 border bg-card p-4">
+		<h1 class="text-lg font-semibold">pager</h1>
+		<Input placeholder="short description*" />
+		<Textarea placeholder="more details"></Textarea>
+		<Button type="submit">Submit</Button>
+	</div>
+</div>
