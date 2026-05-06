8fc1d2f4f36839a2f29a08755f62d69d30e31db7
Ingo Wolf <me@ingo.au>
2026-05-07 01:48:43 +1000

    add auth page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { FingerprintSimpleIcon } from 'phosphor-svelte';
+</script>
+
+<div class="w-full p-4">
+	<div class="mx-auto flex w-full max-w-xl flex-col items-center gap-2 border bg-card p-4">
+		<FingerprintSimpleIcon class="size-10" />
+		authenticate to access
+	</div>
+</div>
