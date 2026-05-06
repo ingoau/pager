5f2e845f1c03adac520e99dfbad57a039ae8aac6
Ingo Wolf <me@ingo.au>
2026-05-07 01:04:57 +1000

    add confirmation and errors

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,5 +3,9 @@ import type { Actions } from './$types';
 export const actions = {
 	default: async (event) => {
 		console.log(event);
+
+		return {
+			success: true
+		};
 	}
 } satisfies Actions;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,12 @@
 	import * as AlertDialog from '$lib/components/ui/alert-dialog/index.js';
 	import { WarningIcon } from 'phosphor-svelte';
 
-	let form: HTMLFormElement;
+	let formElement: HTMLFormElement;
 
 	let confirmDialogOpen = $state(false);
 	let confirmed = $state(false);
+
+	const { form } = $props();
 
 	function handleSubmit(event: SubmitEvent) {
 		if (!confirmed) {
@@ -22,51 +24,63 @@
 	function confirmSubmit() {
 		confirmed = true;
 		confirmDialogOpen = false;
-		form.requestSubmit();
+		formElement.requestSubmit();
 		confirmed = false;
 	}
 </script>
 
-<div class="w-full p-4">
-	<form
-		class="mx-auto w-full max-w-xl space-y-4 border bg-card p-4"
-		bind:this={form}
-		onsubmit={handleSubmit}
-		method="POST"
-	>
-		<h1 class="text-lg font-semibold">pager</h1>
-		<Input required placeholder="short description*" name="description" />
-		<Textarea placeholder="more details" name="details"></Textarea>
-		<RadioGroup.Root required value="low" name="priority">
-			<div class="flex items-center space-x-2">
-				<RadioGroup.Item value="low" id="low" />
-				<Label for="low">Low priority</Label>
-			</div>
-			<div class="flex items-center space-x-2">
-				<RadioGroup.Item value="high" id="high" />
-				<Label for="high">High priority</Label>
-			</div>
-		</RadioGroup.Root>
-		<Button type="submit">Submit</Button>
-	</form>
-</div>
+{#if form}
+	{#if form.success}
+		<div class="w-full p-4">
+			<p>Success!</p>
+		</div>
+	{:else if !form.success}
+		<div class="w-full p-4">
+			<p>Something went wrong</p>
+		</div>
+	{/if}
+{:else}
+	<div class="w-full p-4">
+		<form
+			class="mx-auto w-full max-w-xl space-y-4 border bg-card p-4"
+			bind:this={formElement}
+			onsubmit={handleSubmit}
+			method="POST"
+		>
+			<h1 class="text-lg font-semibold">pager</h1>
+			<Input required placeholder="short description*" name="description" />
+			<Textarea placeholder="more details" name="details"></Textarea>
+			<RadioGroup.Root required value="low" name="priority">
+				<div class="flex items-center space-x-2">
+					<RadioGroup.Item value="low" id="low" />
+					<Label for="low">Low priority</Label>
+				</div>
+				<div class="flex items-center space-x-2">
+					<RadioGroup.Item value="high" id="high" />
+					<Label for="high">High priority</Label>
+				</div>
+			</RadioGroup.Root>
+			<Button type="submit">Submit</Button>
+		</form>
+	</div>
 
-<AlertDialog.Root bind:open={confirmDialogOpen}>
-	<AlertDialog.Content>
-		<AlertDialog.Header>
-			<WarningIcon class="size-8" />
-			<AlertDialog.Title>are you sure?</AlertDialog.Title>
-			<AlertDialog.Description>
-				is this actually something important? do i really need to be notified about this now? could
-				you just dm me?
-				<br />
-				<br />
-				(any misuse may result in you being banned from this service)
-			</AlertDialog.Description>
-		</AlertDialog.Header>
-		<AlertDialog.Footer>
-			<AlertDialog.Cancel>cancel</AlertDialog.Cancel>
-			<AlertDialog.Action onclick={confirmSubmit}>continue</AlertDialog.Action>
-		</AlertDialog.Footer>
-	</AlertDialog.Content>
-</AlertDialog.Root>
+	<AlertDialog.Root bind:open={confirmDialogOpen}>
+		<AlertDialog.Content>
+			<AlertDialog.Header>
+				<WarningIcon class="size-8" />
+				<AlertDialog.Title>are you sure?</AlertDialog.Title>
+				<AlertDialog.Description>
+					is this actually something important? do i really need to be notified about this now?
+					could you just dm me?
+					<br />
+					<br />
+					(any misuse may result in you being banned from this service)
+				</AlertDialog.Description>
+			</AlertDialog.Header>
+			<AlertDialog.Footer>
+				<AlertDialog.Cancel>cancel</AlertDialog.Cancel>
+				<AlertDialog.Action onclick={confirmSubmit}>continue</AlertDialog.Action>
+			</AlertDialog.Footer>
+		</AlertDialog.Content>
+	</AlertDialog.Root>
+{/if}
